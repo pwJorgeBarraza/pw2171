c222090d1b127bfe7157ce2f3350d55fb36867dd
pwJorgeBarraza <jofer.bz20@gmail.com>
2017-04-03 14:59:59 -0600

    JavaScript

---
+++ js/sumas.js
@@ -0,0 +1,16 @@
+function suma(a,b)
+		{
+			return a+b;
+		}
+		var suma2 = function(a,b){
+			return a+b;
+		}
+function texto(){
+	alert(document.getElementById("texto1").value)
+}
+function suma3(){
+	var 1 = document.getElementById("texto1").value;
+	var 2 = document.getElementById("texto2").value;
+	var 3 = 1+2;
+	alert(3);
+}
