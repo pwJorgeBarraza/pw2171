409668a5d29362b4da1b22086dab12e7a41d56c7
pwJorgeBarraza <jofer.bz20@gmail.com>
2017-05-08 14:56:44 -0600

    php prueba

---
+++ pw2171server/prueba.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP :(</title>
+</head>
+<body>
+	<?php
+	for ($i=0; $i < 10; $i++) { 
+		print( "hola php <br>");
+	}
+	?>
+</body>
+</html>
---
+++ pw2171server/validausuario.php
@@ -0,0 +1,6 @@
+<?php
+	$usuario= $_POST["txtUsuario"];
+	$clave  = $_POST["txtClave"];
+	print($usuario);
+	print($clave);
+?>
